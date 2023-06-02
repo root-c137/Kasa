90b31434d26dd5c8196b24442c704296670621dd
rootc137 <pessicksanchez@gmail.com>
2023-06-02 14:49:45 +0200

    Gallery prototype..

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.scss';
 
 import {Header} from "./Components/Header/Header";
 import {Presentation} from "./Components/Presentation/Presentation";
+import {Gallery} from "./Components/Gallery/Gallery";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Presentation />
+      <Gallery />
     </div>
   );
 }
---
+++ src/Components/Gallery/Gallery.js
@@ -0,0 +1,39 @@
+
+
+import "./Gallery.scss";
+
+
+export const Gallery = () =>
+{
+    return(
+        <section className="Gallery">
+
+            <div className="Gallery__Card">
+                <img className="Card__Img" src="" />
+                <h2 className="Card__Title">Titre de la location</h2>
+            </div>
+            <div className="Gallery__Card">
+                <img className="Card__Img" src="" />
+                <h2 className="Card__Title">Titre de la location</h2>
+            </div>
+            <div className="Gallery__Card">
+                <img className="Card__Img" src="" />
+                <h2 className="Card__Title">Titre de la location</h2>
+            </div>
+            <div className="Gallery__Card">
+                <img className="Card__Img" src="" />
+                <h2 className="Card__Title">Titre de la location</h2>
+            </div>
+            <div className="Gallery__Card">
+                <img className="Card__Img" src="" />
+                <h2 className="Card__Title">Titre de la location</h2>
+            </div>
+            <div className="Gallery__Card">
+                <img className="Card__Img" src="" />
+                <h2 className="Card__Title">Titre de la location</h2>
+            </div>
+
+        </section>
+    );
+}
+
