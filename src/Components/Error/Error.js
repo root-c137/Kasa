f50ea3b180264de0e0b11f286d9ee233f2efb7f3
rootc137 <pessicksanchez@gmail.com>
2023-06-04 14:45:18 +0200

    Page error responsive..

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,13 +1,16 @@
 
 
 import "./Error.scss";
+import {Link} from "react-router-dom";
 
 export const Error = () =>
 {
     return (
         <div className="Error">
-            <p className="Error__Txt">404</p>
-            <p className="Error__Txt">Oups il n'y a rien par ici...</p>
+            <p className="Error__Title">404</p>
+            <p className="Error__Txt">Oups! La page que vous demandez n'existe pas.</p>
+
+            <Link className="Error__Link" to="/">Retourner sur la page d'accueil</Link>
         </div>
     );
 }
