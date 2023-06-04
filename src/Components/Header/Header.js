8743882181bceab48d49e87476b64a5e1ee8778e
rootc137 <pessicksanchez@gmail.com>
2023-06-04 14:27:04 +0200

    header currentlink underline..

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,41 @@
 
 import './Header.scss';
-import { Link } from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 
 import Logo from "../../Images/LOGO.svg";
+import {useEffect, useState} from "react";
 export const Header = () =>
 {
+    const Location = useLocation();
+    const CurrentLinkClass = "Nav__Link--CurrentLink";
+    const [AddClassHome, setAddClassHome] = useState("");
+    const [AddClassApropos, setAddClassAPropos] = useState("");
+
+
+    useEffect( () =>
+    {
+        console.log(Location.pathname);
+
+        if(Location.pathname === "/")
+        {
+            setAddClassHome(CurrentLinkClass);
+            setAddClassAPropos("");
+        }
+        if(Location.pathname === "/a-propos")
+        {
+            setAddClassAPropos(CurrentLinkClass);
+            setAddClassHome("");
+        }
+    })
+
+
     return(
         <header>
             <img className="Header__Logo" src={Logo} />
             <nav className="Header__Nav">
-                <Link className="Nav__Link Nav__Link--CurrentLink" to="/">Accueil</Link>
-                <Link className="Nav__Link" to="a-propos">A Propos</Link>
+                <Link className={"Nav__Link "+AddClassHome} to="/">Accueil</Link>
+        <Link className={"Nav__Link "+AddClassApropos} to="a-propos">A Propos</Link>
             </nav>
         </header>
     );
